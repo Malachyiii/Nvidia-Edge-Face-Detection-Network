271f7eee0093608ac274e9b25165a3193a9baf51
Malachy Moran <malachy.j.moran@berkeley.edu>
2021-09-09 17:47:58 -0800

    First pass Image recognition

--- classifier.py
+++ classifier.py
@@ -1,13 +1,33 @@
 import numpy as np
 import cv2 as cv
+import paho.mqtt.client as mqtt
+
+LOCAL_MQTT_HOST="localhost"
+LOCAL_MQTT_PORT=1833
+LOCAL_MQTT_TOPIC="test_topic"
+
+def on_connect_local(client, userdata, flags, rc):
+        print("connected to local broker with rc: " + str(rc))
+
+local_mqttclient = mqtt.Client()
+local_mqttclient.on_connect = on_connect_local
+local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
+
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# gray here is the gray frame you will be getting from a camera
-gray = cv.cvtColor(gray, cv.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-	# your logic goes here; for instance
-	# cut out face from the frame.. 
-	# rc,png = cv2.imencode('.png', face)
-	# msg = png.tobytes()
-	# ...
+cap = cv.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+    # gray here is the gray frame you will be getting from a camera
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+    	# your logic goes here; for instance
+    	# cut out face from the frame..
+        face = frame[y:y+h, x:x+w]
+        rc,png = cv.imencode('.png', face)
+        msg = png.tobytes()
+        local_mqttclient.publish(LOCAL_MQTT_TOPIC, msg)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
