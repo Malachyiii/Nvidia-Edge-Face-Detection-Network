f72b72f20d3258bc28e735b3b4568d545231e3f9
Malachy Moran <malachy.j.moran@berkeley.edu>
2021-09-11 22:55:51 -0800

    done for tonight

--- classifier_run.sh
+++
@@ -1,3 +0,0 @@
-docker build -t malachyiii/classifier -f classifier_Dockerfile .
-docker push malachyiii/classifier
-kubectl apply -f classifier.yaml
--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@ docker push malachyiii/broker
 docker build -t malachyiii/listener -f listener_Dockerfile .
 docker push malachyiii/listener
 docker build -t malachyiii/classifier -f classifier_Dockerfile .
+docker push malachyiii/classifier
 sudo systemctl start k3s
 export DISPLAY=:0
 xhost +
 kubectl apply -f mosquitto.yaml
 kubectl apply -f mosquittoService.yaml
+kubectl apply -f classifier.yaml
 kubectl apply -f listener.yaml
 
