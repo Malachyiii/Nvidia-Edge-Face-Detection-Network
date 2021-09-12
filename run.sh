3b8f9aa5b0f9d52398b9f0599abfe39147958c1e
Malachy Moran <malachy.j.moran@berkeley.edu>
2021-09-11 17:25:26 -0800

    updated listener Dockerfile

---
+++ run.sh
@@ -0,0 +1,14 @@
+docker build -t malachyiii/broker -f broker_Dockerfile .
+docker push malachyiii/broker
+docker build -t malachyiii/listener -f listener_Dockerfile .
+docker push malachyiii/listener
+docker build -t malachyiii/classifier -f classifier_Dockerfile .
+docker push malachyiii/classifier
+sudo systemctl start k3s
+export DISPLAY=:0
+xhost +
+kubectl apply -f mosquitto.yaml
+kubectl apply -f mosquittoService.yaml
+kubectl apply -f listener.yaml
+kubectl apply -f classifier.yaml
+
