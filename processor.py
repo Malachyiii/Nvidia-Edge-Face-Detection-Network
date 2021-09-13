bc98a08746ff4ecb82e3aafb77415b0b9252d671
root <root@ip-172-31-32-221.us-west-2.compute.internal>
2021-09-13 03:55:53 +0000

    just working on s3 now

--- processor.py
+++ processor.py
@@ -11,12 +11,19 @@ def on_connect_local(client, userdata, flags, rc):
         print("connected to local broker with rc: " + str(rc))
         client.subscribe(LOCAL_MQTT_TOPIC)
 	
+image_num = 0
+
 def on_message(client,userdata, msg):
+  global image_num
   try:
     print("message received")
     image = np.frombuffer(msg.payload, dtype="uint8")
     image = cv.imdecode(image, cv.IMREAD_COLOR)
     print("image decoded")
+    path = '/var/www/s3/'+str(image_num)+'.png'
+    cv.imwrite(path, image, [cv.IMWRITE_PNG_COMPRESSION,4])
+    image_num += 1
+    print("image written")
   except:
     print("Unexpected error:", sys.exc_info()[0])
 
