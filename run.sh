e9d041eee504ef21ba1636933efae131fc5cc7a3
Malachy Moran <malachy.j.moran@berkeley.edu>
2021-09-12 16:00:15 -0800

    adding republisher

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@ docker build -t malachyiii/broker -f broker_Dockerfile .
 docker push malachyiii/broker
 docker build -t malachyiii/listener -f listener_Dockerfile .
 docker push malachyiii/listener
+docker build -t malachyiii/repub -f repub_Dockerfile .
+docker push malachyiii/repub
 docker build -t malachyiii/classifier -f classifier_Dockerfile .
 docker push malachyiii/classifier
 sudo systemctl start k3s
