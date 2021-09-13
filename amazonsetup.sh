583b25200dc906505183d6336ea481bc81b6530c
Malachy Moran <malachy.j.moran@berkeley.edu>
2021-09-12 20:36:33 -0800

    mission accomplished

--- amazonsetup.sh
+++ amazonsetup.sh
@@ -1,11 +1,14 @@
 ssh -A ubuntu@ec2-34-218-45-186.us-west-2.compute.amazonaws.com
 cd ~/W251_Homework3
 sudo su
+
+s3fs -o iam_role="HomeworkRole" -o url="https://s3-us-west-2.amazonaws.com" -o endpoint=us-west-2 -o dbglevel=info -o curldbg -o allow_other -o use_cache=/tmp w251homework3 /var/s3fs-homework
+
 docker build -t malachyiii/aws_broker -f aws_mqtt_Dockerfile .
 docker push malachyiii/aws_broker
 docker build -t malachyiii/processor -f processor_Dockerfile .
 docker push malachyiii/processor
 docker network create image-storage
-
-docker run -d -it -p 1883:1883 -p 9001:9001 --network image-storage -v /home/ubuntu/W251_Homework3:/mosquitto/config/mosquitto.conf -v /home/ubuntu/W251_Homework3:/mosquitto/data -v /home/ubuntu/W251_Homework3/mosquitto/log:/mosquitto/log malachyiii/aws_broker
-docker run -d -it --network image-storage malachyiii/processor
+docker run -d --name aws_broker -it -p 1883:1883 --network image-storage -v /home/ubuntu/W251_Homework3:/mosquitto/config/mosquitto.conf -v /home/ubuntu/W251_Homework3:/mosquitto/data -v /home/ubuntu/W251_Homework3/mosquitto/log:/mosquitto/log malachyiii/aws_broker
+sleep 10
+docker run -d --privileged --name processor -i -v /var/s3fs-homework:/s3 --network image-storage malachyiii/processor
